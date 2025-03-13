dbe4995c4ebac21f9c92b8e848b715397b723d70
ImagicaQueen <khushichandravansi3@gmail.com>
2025-03-13 19:37:18 +0530

    user registration done

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { body } = require("express-validator");
+const router = express.Router();
+const userController = require("../controllers/user.controller");
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullname.firstname") 
+    .optional()
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characters long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  userController.registerUser
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  userController.loginUser
+);
+
+router.get("/profile", userController.getUserProfile)
+
+module.exports = router;
